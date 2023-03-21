90bfb0a5cb6291e499c560531cd1a84490722327
kgg75 <kgg75@mail.ru>
2023-03-21 22:02:55 +0700

    Update Main.cpp

--- Server/Main.cpp
+++ Server/Main.cpp
@@ -1,4 +1,5 @@
-﻿#include <windows.h>
+#include <windows.h>
+#include <thread>
 #include "Server.h"
 
 
@@ -12,21 +13,11 @@ int main() {
 	if (iResult != 0)
 		return iResult;
 
-	const auto THREADS_NUMBER = 2;
-	HANDLE hThreads[THREADS_NUMBER];
-	hThreads[0] = CreateThread(0, 0, (LPTHREAD_START_ROUTINE)ChatCommandsHandler, 0, 0, 0);	// обработчик команд чата
-	hThreads[1] = CreateThread(0, 0, (LPTHREAD_START_ROUTINE)ChatServer, 0, 0, 0);	// главный обработчик чата
+	thread chatCommandsThread(ChatCommandsHandler);
+	iResult = ChatServer();
 
-	if (hThreads[0] == 0 || hThreads[1] == 0)
-		return 1;	// ошибка!
+	if (chatCommandsThread.joinable())
+		chatCommandsThread.join();
 
-	WaitForMultipleObjects(THREADS_NUMBER, hThreads, TRUE, INFINITE);
-
-	DWORD iRetVal[THREADS_NUMBER];
-	if (GetExitCodeThread(hThreads[0], &iRetVal[0]))
-		CloseHandle(hThreads[0]);
-	if (GetExitCodeThread(hThreads[1], &iRetVal[1]))
-		CloseHandle(hThreads[1]);
-
-	return iRetVal[1];	// результат ChatServer()
+	return iResult;
 }
